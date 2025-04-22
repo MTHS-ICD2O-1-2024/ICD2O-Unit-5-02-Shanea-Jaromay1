cba2e499251cd450730ad1e00f4979264bec467f
Shanea-Jaromay1 <shanea.jaromay@stu.ocsb.ca>
2025-04-22 17:32:57 +0000

    init

--- js/script.js
+++ js/script.js
@@ -1,5 +1,28 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2025 Shanea Jaromay All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Shanea Jaromay
+// Created on: Apr 2025
 // This file contains the JS functions for index.html
+
+const randomNumber = Math.floor(Math.random() * 6) + 1
+
+/**
+ * This function checks if the user has selected the random number from above
+ */
+// eslint-disable-next-line no-unused-vars
+function generateNumber () {
+  // input
+  const positiveNumber = document.getElementById('option-positive').checked
+  // process
+  if (positiveNumber == true) {
+    // output
+    document.getElementById('answer').innerHTML =
+    'You have guessed the correct number! HURRAY!'
+  }
+  if (userNumber !== randomNumber) {
+    // output
+    document.getElementById('result').innerHTML =
+    'You have guessed the wrong number! BOO!' +
+    'The correct number was:' + randomNumber + '.'
+  }
+}
